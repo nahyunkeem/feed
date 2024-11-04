72c84545558d6f00d0c68814e3247acdeea23e69
sangah <kimsangah_@naver.com>
2024-11-04 22:39:20 +0900

    feat: user 모델과 auth_code 모델 생성

--- feed/accounts/models.py
+++ feed/accounts/models.py
@@ -1,5 +1,25 @@
+from datetime import timedelta, timezone
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import random
+
 
 class CustomUser(AbstractUser):
-    email = models.EmailField("email address")
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField("email address", unique=True)
+    first_name = None
+    last_name = None
+    is_active = models.BooleanField(default=False)
+
+
+class AuthCode(models.Model):
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    auth_code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expired_at = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        self.expired_at = timezone.now() + timedelta(days=1)
+        self.auth_code = random.randint(100000, 999999)
+        # print(self.auth_code)
+        super().save(*args, **kwargs)
