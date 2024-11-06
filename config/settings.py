abb737dd5679a85ca6fb03e9e9f38130d847ba7e
sangah <kimsangah_@naver.com>
2024-11-06 22:10:20 +0900

    :tada: add: 비밀번호 검증 추가

--- config/settings.py
+++ config/settings.py
@@ -93,7 +93,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    "OPTIONS" : {"min_length" : 10,}
+        "OPTIONS" : {"min_length" : 10,}
     },
     {
         "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
--- feed/accounts/serializers.py
+++ feed/accounts/serializers.py
@@ -1,13 +1,24 @@
-from rest_framework import serializers
-from django.contrib.auth import get_user_model
 from .models import AuthCode
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 CustomUser = get_user_model()
+
+
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['username', 'email', 'password']
-        
+
+    def validate_password(self, value):
+        try:
+            validate_password(value)
+        except ValidationError as e:
+            raise e
+        return value
+
     def create(self, validated_data):
         password = validated_data.pop('password')
         user = CustomUser(**validated_data)
@@ -15,4 +26,3 @@ class CustomUserSerializer(serializers.ModelSerializer):
         user.save()
         AuthCode.objects.create(user_id=user)
         return user
-    
