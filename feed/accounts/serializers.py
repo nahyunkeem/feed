51d26b87eeb081ac2ce10095031b55e682e85f77
test <>
2024-11-07 22:02:44 +0900

    feat: 사용자 인증

--- feed/accounts/serializers.py
+++ feed/accounts/serializers.py
@@ -1,6 +1,9 @@
+from django.utils import timezone
+from django.forms import ValidationError
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from .models import AuthCode
+from django.contrib.auth.hashers import check_password
 
 CustomUser = get_user_model()
 class CustomUserSerializer(serializers.ModelSerializer):
@@ -9,10 +12,46 @@ class CustomUserSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password']
         
     def create(self, validated_data):
-        password = validated_data.pop('password')
-        user = CustomUser(**validated_data)
+        # 해싱 되지 않는 비밀번호를 삭제하고 해싱시켜 다시 저장
+        password = validated_data.pop('password', None)
+        user = CustomUser(**validated_data)       # 비밀번호를 제외한 데이터 
         user.set_password(password)
         user.save()
+        
+        # 인증 코드 생성
         AuthCode.objects.create(user_id=user)
         return user
     
+        # 계정 데이터 인증
+    def auth_validate(self, data):
+        username = data.get('username')
+        password = data.get('password')
+        auth_code = data.get('auth_code')
+
+        try:
+            user=CustomUser.objects.get(username=username)
+        except CustomUser.DoesNotExist:
+            raise serializers.ValidationError("없는 계정입니다.") 
+        
+        if not user.check_password(password):
+            raise serializers.ValidationError("비밀번호가 올바르지 않습니다.")
+        
+        
+
+        # 인증 코드 체크
+        try:
+            auth_code_obj = AuthCode.objects.get(
+                user_id=user,
+                auth_code=auth_code,
+                expired_at__gt=timezone.now()       # 인증 코드가 만료되지 않았나 확인
+            )
+        except AuthCode.DoesNotExist:
+            raise serializers.ValidationError("유효하지 않은 인증 코드입니다.")
+
+        # 인증 완료 후 인증 코드 삭제 
+        auth_code_obj.delete()
+
+        user.is_active = True
+        user.save()
+
+        return data
--- feed/accounts/views.py
+++ feed/accounts/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
 from .serializers import CustomUserSerializer
 
 
@@ -8,5 +9,13 @@ class SignUpAPIView(APIView):
         serializer = CustomUserSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
-            return Response(status=201)
-
+            return Response({
+                "message": "사용자 등록이 완료되었습니다."
+            }, status=status.HTTP_201_CREATED)
+        
+    def patch(self, request):
+        serializer = CustomUserSerializer()
+        if serializer.auth_validate(request.data):
+            return Response({"message": "사용자 인증이 완료되었습니다."})
+        
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
