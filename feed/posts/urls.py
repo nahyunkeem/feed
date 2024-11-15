bf8a57cce4475557305e769ace17a3d368b79f61
sangah <kimsangah_@naver.com>
2024-11-15 11:59:57 +0900

    :recycle: refact: 게시물 검색 코드 수정

--- feed/posts/urls.py
+++ feed/posts/urls.py
@@ -1,8 +1,7 @@
-
-from django.urls import path, include
+from django.urls import path
 from .views import PostListView, PostListAPIView
 
 urlpatterns = [
     path("", PostListView.as_view(), name="post_list"),
-    path("search/", PostListAPIView.as_view()),
+    path("search", PostListAPIView.as_view()),
 ]
